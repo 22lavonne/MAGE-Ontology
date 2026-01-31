ef607c275e1166786625347ca5deb05aad125cd6
22lavonne <22lavonne@gmail.com>
2026-01-31 13:14:12 -0500

    added extraction for symbols and variables

--- ghidra-scripting/data-extraction.py
+++ ghidra-scripting/data-extraction.py
@@ -14,35 +14,41 @@ from pathlib import Path
 # get all the symbols and their addresses
 # maybe put all types of symbols into their own classes?
 
-# function names and entry points:
-# with open('/wsl.localhost/Ubuntu/home/emiller/MKG/Ghidra-Scripting/output.txt', 'w+') as f:
-# out_path = Path("\\wsl$\Ubuntu\home\emiller\MKG\Ghidra-Scriptin\goutput.txt")
-# with out_path.open("w", encoding="utf-8") as f:
-# with open("\\wsl$\Ubuntu\home\emiller\MKG\Ghidra-Scriptin\goutput.txt", "w+", encoding="utf-8") as f:
-# with redirect_stdout(f):
+# gets path of where this script is actually located so any extracted txt files can be put in this directory
 script_dir = Path(getSourceFile().getAbsolutePath()).parent
-func_out_path = script_dir / "function-output.txt"
+
 
 # TODO: figure out how to get all the variables from each function
-# returns all functions in function_output.txt
+# prints all function names and their entry points entry points into function.txt
+func_out_path = script_dir / "function-output.txt"
+var_out_path = script_dir / "variable-output.txt"
 with func_out_path.open("w", encoding="utf-8") as f:
     function = getFirstFunction()
-    while function is not None:
-        f.write("Function name: " + function.getName() + " Function Entry Point: " + str(function.getEntryPoint()) + "\n")
-        function = getFunctionAfter(function)
+    with var_out_path.open("w", encoding="utf-8") as f2:
+        while function is not None:
+            f.write("Function name: " + function.getName() + " Function Entry Point: " + str(function.getEntryPoint()) + "\n")
+            var_array = function.getAllVariables()
+            for variable in var_array:
+                # can also use getFunction() method to get the function the variable is located in
+                f2.write("Variable from " + str(function) + ": " + variable.getName() + ": " + str(variable) + "\n")
+            function = getFunctionAfter(function)
 
-# returns all instructions in instruction_output.txt
+# prints all instructions in instruction-output.txt
 ins_out_path = script_dir / "instruction-output.txt"
-
-
 with ins_out_path.open("w", encoding="utf-8") as f:
     instruction = getFirstInstruction()
     while instruction is not None:
         f.write("Instruction: " + str(instruction) + "\n")
         instruction = getInstructionAfter(instruction)
 
+# prints all symbols in symbol-output.txt
+symbol_out_path = script_dir / "symbol-output.txt"
+with symbol_out_path.open("w", encoding="utf-8") as f:
+    symbol_iterator = currentProgram.getSymbolTable().getAllSymbols(True)
+    for s in symbol_iterator:
+        f.write("Symbol: " + str(s) + " address: " + str(s.getAddress()) +  "\n")
+
+
+
 # notes about instructions:
 # ghidra api website: https://ghidra.re/ghidra_docs/api/index.html 
-# getResultObjects() function is not recognized, even though it shows up in ghidra API website
-# getOperandType() only works if you have the operand index, same with getOpObjects()
-# so i need to get a way to find all the operand indexes
