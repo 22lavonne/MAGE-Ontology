a8ed1436fe47823b8dd895e22585000134ff0523
22lavonne <22lavonne@gmail.com>
2026-02-03 14:00:10 -0500

    added method to print all references for a given symbol, finished fixing formatting for all symbol output text files

--- ghidra-scripting/data-extraction.py
+++ ghidra-scripting/data-extraction.py
@@ -9,25 +9,15 @@ from pathlib import Path
 from ghidra.program.model.symbol import SymbolType
 
 # TODO:
-# I should probably make 2 subclasses for variables: local variables and parameters
-# then change like everything relating to variables to be for local or parameters instead (INCLUDING ONTOLOGY)
+# finish updating the formatting of variables
+# finsih the formatting of the other output files
+# make a function to print the references instead of copy and pasting everything
 
-# Finish fixing the formatting of all the output.txt files
-
-# see what else you need to include for all symbols based on schema and ontology and add them in too (once you start scripting)
-
-# look into the differences between variables in functions and SymbolType.PARAMETER, SymbolType.LOCAL_VAR, and SymbolType.GLOBAL_VAR
-    # also see if variables should always have a label associated with them
+# see what else you need to include for all symbols based on schema and ontology and add them in too (once you start string parsing)
 
 # update key notions and axiomization with new schema
-# modify script if necessary when working on string parsing
 # if undefined does not work for a data type, change it to something that can work 
     # (functions can return both void and undefined)
-
-# NOTE: if all dlls have a parent namespace of global, should that be hard coded into the ontology somehow?
-
-# Types of Namespaces: namespace, class, library, function, global (the global namespace)
-# TODO: should I add another object for the global namespace?
 
 
 def main():
@@ -37,59 +27,45 @@ def main():
     # prints all label symbols in label-output.txt
     label_out_path = script_dir / "label-output.txt"
     func_out_path = script_dir / "function-output.txt"
-    var_out_path = script_dir / "variable-output.txt"
+    local_var_out_path = script_dir / "local-variable-output.txt"
+    param_out_path = script_dir / "parameter-output.txt"
     with label_out_path.open("w", encoding="utf-8") as label_file:
         with func_out_path.open("w", encoding="utf-8") as func_file:
-            with var_out_path.open("w", encoding="utf-8") as var_file:
-                symbol_iterator = currentProgram.getSymbolTable().getSymbolIterator()
-                for s in symbol_iterator:
-                    file_to_write = label_file
-                    # if current symbol type is a function, print in function file
-                    if (s.getSymbolType() == SymbolType.FUNCTION):
-                        # func_file.write("Function: " + s.getName() + " Addesss: " + str(s.getAddress()) + " Parent Namespace: " + str(s.getParentNamespace()) + " References: ")
-                        # TODO: get return type of function, and what functions this function calls
-                        func_file.write("FUNCTION func=" + s.getName() + " address:" + str(s.getAddress()) + " parent=" + str(s.getParentNamespace()) + " returntype=" + str(s.getReturnType()) + "\n")
-                        # TODO: do a similar thing here with called functions that you do with references
-                        func_array = s.getCalledFunctions(monitor)
-                        if func_array:
-                            for func in func_array:
-                                # NOTE: should I add more identification to the functions here besides the name?
-                                func_file.write("FUNCTIONCALLED func=" + func.getName() + "\n")                        
-                        # then print the corresponding variables from that function
-                        # since s is currently of type Function Symbol (which is just the naming mechanism) 
-                        # and not Function (which represents the actual function), we have to get the function object from the symbol
-                        # in order to call the method getAllVariables() on it.
-                        var_array = s.getObject().getAllVariables()
-                        for var in var_array:
-                            # NOTE: add address here if needed, but it is currently not on schema
-                            # var_file.write("Variable: " + var.getName() +  " Defined in: " + str(s) + " Data type: " + str(var.getDataType()) + "\n")
-                            var_file.write("VARIABLE var=" + var.getName() + " datatype=" + str(var.getDataType()) + " parent=" + str(s) + "\n")
-                        local_array = s.getObject().getLocalVariables()
-                        param_array = s.getObject().getParameters()
-                        # TODO: should parameters and local variables be subclasses of type variable?
-                        # change this to f2 so the references print to the function output file for this symbol
-                        file_to_write = func_file
-                    # if it's not a function, then it's a label
-                    else:
-                        # label_file.write("LABEL Label: " + s.getName() + " Addresss: " + str(s.getAddress()) + " Parent Namespace: " + str(s.getParentNamespace()) + " References: ")
-                        label_file.write("LABEL label=" + s.getName() + " address:" + str(s.getAddress()) + " parent=" + str(s.getParentNamespace()) + "\n")
+            with local_var_out_path.open("w", encoding="utf-8") as local_file:
+                with param_out_path.open("w", encoding="utf-8") as param_file:
+                    symbol_iterator = currentProgram.getSymbolTable().getSymbolIterator()
+                    for s in symbol_iterator:
                         file_to_write = label_file
-                    # then, no matter if it was a function or label, print its references
-                    ref_array = s.getReferences()
-                    # if there is at least one reference, then print the references.
-                    # if there is not, then no references will be printed.
-                    if ref_array:
-                        primary_reference = ""
-                        for reference in ref_array:
-                            if reference.isPrimary():
-                                primary_reference = str(reference)
-                            # file_to_write.write(str(reference) + ",")
-                            file_to_write.write("REFERENCE source=" + str(reference.getFromAddress()) + " destination=" + str(reference.getToAddress()) + " operandindex=" + str(reference.getOperandIndex()) + " type=" + str(reference.getReferenceType()) + "\n")
-                        file_to_write.write("PRIMARYREFERENCE source=" + str(reference.getFromAddress()) + " destination=" + str(reference.getToAddress()) + " operandindex=" + str(reference.getOperandIndex()) + " type=" + str(reference.getReferenceType()) + "\n")
-    # variable format:
-    # [undefined4 local_8@Stack[-0x8]:4]
-    # [data_type variable_name@stack[location]:#_of_bits?]
-
+                        # if current symbol type is a function, print in function file
+                        if (s.getSymbolType() == SymbolType.FUNCTION):
+                            # TODO: change the formatting of the return parameter if necessary 
+                            func_file.write("FUNCTION func=" + s.getName() + " address:" + str(s.getAddress()) + " parent=" + str(s.getParentNamespace()) + " returntype=" + str(s.getObject().getReturnType()) + " returnvalue=" + str(s.getObject().getReturn()) + "\n")
+                            func_array = s.getObject().getCalledFunctions(monitor)
+                            if func_array:
+                                for func in func_array:
+                                    func_file.write("FUNCTIONCALLED func=" + func.getName() + "\n")                        
+                            # then print the corresponding variables from that function
+                            # both the local variables and the parameters
+                            local_array = s.getObject().getLocalVariables()
+                            if local_array:
+                                for local in local_array:
+                                    local_file.write("LOCALVARIABLE var=" + local.getName() + " datatype=" + str(local.getDataType()) + " parent=" + str(s) + "\n")
+                            param_array = s.getObject().getParameters()
+                            if param_array:
+                                for param in param_array:
+                                    #TODO: fix the formatting of the data type because it has spaces in between some (for like pointers and stuff)
+                                    param_file.write("PARAMETER var=" + param.getName() + " datatype=" + str(param.getDataType()) + " parent=" + str(s) + "\n")
+                            
+                            file_to_write = func_file
+                        # if it's not a function, then it's a label
+                        else:
+                            label_file.write("LABEL label=" + s.getName() + " address:" + str(s.getAddress()) + " parent=" + str(s.getParentNamespace()) + "\n")
+                            file_to_write = label_file
+                        # then, no matter if it was a function or label, print its references
+                        ref_array = s.getReferences()
+                        # if there is at least one reference, then print the references.
+                        # if there is not, then no references will be printed.
+                        print_references(ref_array, file_to_write)
 
     # prints all instructions in instruction-output.txt
     # TODO: use methods for instructions to make parsing easier if necessary
@@ -109,15 +85,10 @@ def main():
             # NOTE: classes do not have any references when trying to access them through the ghidra api
             # this is because references to classes are often indirect
             # so if I want classes to be able to have references, I might have to do more digging to find them
-            f.write("Class: " + str(c) + " Address: " + str(c.getSymbol().getAddress()) + " Parent Namespace: " + str(c.getSymbol().getParentNamespace()) + " References: ")
+            f.write("CLASS class=" + str(c) + " address=" + str(c.getSymbol().getAddress()) + " parent=" + str(c.getSymbol().getParentNamespace())  + "\n")
             ref_array = c.getSymbol().getReferences()
-            primary_reference = ""
-            for reference in ref_array:
-                if reference.isPrimary():
-                    primary_reference = str(reference)
-                f.write(str(reference) + ", ")
-            f.write(" Primary reference: " + primary_reference + "\n")
-
+            print_references(ref_array, f)
+            
     # prints all the DLLs in dll-output.txt
     dll_out_path = script_dir / "dll-output.txt"
     # define the external manager here to make other calls shorter
@@ -129,30 +100,22 @@ def main():
             dll_symbol = ext_manager.getExternalLibrary(dll_str).getSymbol()
             # NOTE: all external libraries (which are dlls) will have the parent namespace of global
             # dlls also have no direct references
-            f.write("DLL: " + dll_str + " Address: " + str(dll_symbol.getAddress()) + " Parent Namespace: " + str(dll_symbol.getParentNamespace()) + " References: ")
+            # f.write("DLL: " + dll_str + " Address: " + str(dll_symbol.getAddress()) + " Parent Namespace: " + str(dll_symbol.getParentNamespace()) + " References: ")
+            f.write("DLL dll:" + dll_str + " address:" + str(dll_symbol.getAddress()) + " parent:" + str(dll_symbol.getParentNamespace()) + "\n")
+            
             ref_array = dll_symbol.getReferences()
-            primary_reference = ""
-            for reference in ref_array:
-                if reference.isPrimary():
-                    primary_reference = str(reference)
-                f.write(str(reference) + ", ")
-            f.write(" Primary reference: " + primary_reference + "\n")
-
+            print_references(ref_array, f)
 
     # prints all namespaces into namespace-output.txt
     namespace_out_path = script_dir / "namespace-output.txt"
     with namespace_out_path.open("w", encoding="utf-8") as f:
         namespace_iterator = get_all_namespaces(currentProgram, monitor)
         for namespace in namespace_iterator:
-            f.write("Namespace: " + str(namespace) + " Address: " + str(namespace.getSymbol().getAddress()) + " Parent Namespace: " + str(namespace.getSymbol().getParentNamespace()) + " References: ")
+            # f.write("Namespace: " + str(namespace) + " Address: " + str(namespace.getSymbol().getAddress()) + " Parent Namespace: " + str(namespace.getSymbol().getParentNamespace()) + "\n")
+            f.write("NAMESPACE namespace=" + str(namespace) + " address=" + str(namespace.getSymbol().getAddress()) + " parent=" + str(namespace.getSymbol().getParentNamespace()) + "\n")
             # NOTE: namespaces also do not have direct references
             ref_array = namespace.getSymbol().getReferences()
-            primary_reference = ""
-            for reference in ref_array:
-                if reference.isPrimary():
-                    primary_reference = str(reference)
-                f.write(str(reference) + ", ")
-            f.write(" Primary reference: " + primary_reference + "\n")
+            print_references(ref_array, f)
 
 # function to get all the namespaces of the program
 # (since there is no built in method to do that in the current ghidra api)
@@ -177,6 +140,20 @@ def get_all_namespaces(program, monitor):
 
     traverse_namespaces(global_namespace)
     return all_namespaces
+
+# method to print all the references for a given symbol
+# must pass in the array of references contained for the symbol, and the current file you are writing to
+def print_references(ref_array, file):
+    if ref_array:
+        primary_reference = None
+        for reference in ref_array:
+            if reference.isPrimary():
+                primary_reference = reference
+            # file_to_write.write(str(reference) + ",")
+            file.write("REFERENCE source=" + str(reference.getFromAddress()) + " destination=" + str(reference.getToAddress()) + " operandindex=" + str(reference.getOperandIndex()) + " type=" + str(reference.getReferenceType()) + "\n")
+        if primary_reference:
+            file.write("PRIMARYREFERENCE source=" + str(primary_reference.getFromAddress()) + " destination=" + str(primary_reference.getToAddress()) + " operandindex=" + str(primary_reference.getOperandIndex()) + " type=" + str(primary_reference.getReferenceType()) + "\n")
+    return
 
 if __name__ == "__main__":
     main()
