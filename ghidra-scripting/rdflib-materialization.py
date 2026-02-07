8cac968795d9ded56f95cdece77b0960b08f0ab9
22lavonne <22lavonne@gmail.com>
2026-02-07 17:14:02 -0500

    manually added all object and data properties and classes

--- ghidra-scripting/rdflib-materialization.py
+++ ghidra-scripting/rdflib-materialization.py
@@ -41,10 +41,55 @@ def init_kg(prefixes=pfs):
     return kg
 # rdf:type shortcut
 a = pfs["rdf"]["type"]
-definedIn = pfs["ont"]["definedIn"]
-hasDataType = pfs["ont"]["hasDataType"]
+# definedIn = pfs["ont"]["definedIn"]
+# hasDataType = pfs["ont"]["hasDataType"]
 
-#  add all possible nodes and relations here (because the turtle ontology file cannot be parsed or even used)
+# Object Properties
+definedIn = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/definedIn")
+calls = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/calls")
+hasParameter = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasParameter")
+hasReturnType = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasReturnType")
+passesInto = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/passesInto")
+returns = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/returns")
+containsInstruction = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/containsInstruction")
+hasDataType = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasDataType")
+hasAddress = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasAddress")
+hasSourceAddress = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasSourceAddress")
+hasDestinationAddress = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasDestinationAddress")
+hasReference = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasReference")
+hasPrimaryReference = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasPrimaryReference")
+hasOpcode = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasOpcode")
+hasSourceOperand = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasSourceOperand")
+hasDestinationOperand = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasDestinationOperand")
+performsRole = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/performsRole")
+
+# Data Properties
+addressOf = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/addressOf")
+hasName = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasName")
+hasOperandIndex = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasOperandIndex")
+hasReferenceType = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/hasReferenceType")
+hasType = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/addressOf")
+
+# Classes
+symbol = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Symbol")
+label = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Label")
+namespace_symbol = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/NamespaceSymbol")
+namespace = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Namespace")
+class_ = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Class")
+dll = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/DLL")
+function = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Function")
+variable = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Variable")
+local_variable = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/LocalVariable")
+parameter = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Parameter")
+data_type = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/DataType")
+address = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Address")
+reference = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Reference")
+instruction = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Instruction")
+immediate_operand = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/ImmediateOperand")
+register = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Register")
+opcode = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Opcode")
+operand = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/Operand")
+
 
 # Initialize an empty graph
 graph = init_kg()
@@ -103,17 +148,17 @@ instructions = parse_instructions(instruction_file)
 # TODO: make sure the output for this is actually correct, although idk how to even check that
 # Is it supposed to be a subclass relation thing? I currently have the relations defined above in this file and that's probably incorrect
 # check if this correctly makes local variables a subclass of variable
-LocalVariable = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/LocalVariable")
-for local in local_vars:
-    # use the urllib.parse.quote() method to make the variable name work with URI syntax
-    local_var = pfs["mkg"][quote(local['var'])]
-    data_type = pfs["mkg"][quote(local['datatype'])]
-    parent_func = pfs["mkg"][quote(local['parent'])]
-    graph.add((local_var, a, LocalVariable))
-    graph.add((local_var, definedIn, parent_func))
-    graph.add((local_var, hasDataType, data_type))
+# LocalVariable = URIRef("http://www.semanticweb.org/jaspe/ontologies/2026/0/symbol-ontology/LocalVariable")
+# for local in local_vars:
+#     # use the urllib.parse.quote() method to make the variable name work with URI syntax
+#     local_var = pfs["mkg"][quote(local['var'])]
+#     data_type = pfs["mkg"][quote(local['datatype'])]
+#     parent_func = pfs["mkg"][quote(local['parent'])]
+#     graph.add((local_var, a, LocalVariable))
+#     graph.add((local_var, definedIn, parent_func))
+#     graph.add((local_var, hasDataType, data_type))
     
-    
+ 
     
 
 
